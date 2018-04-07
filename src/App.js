68d609c4d62e3e29b293aad08b96695e6e32819b
Suk Ji Hun <tjrwlgns1198@gmail.com>
2018-04-07 14:23:16 +0900

    revise App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <footer className="App-footer">
+          Chungnam National Universitity(CNU)
+        </footer>
       </div>
     );
   }
