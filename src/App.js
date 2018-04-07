35ea07c991a2d5b5a29624bc287c891601c9a748
Suk Ji Hun <tjrwlgns1198@gmail.com>
2018-04-07 15:43:56 +0900

    modify Add.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SideBar from './SideBar';
+import Content from './Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page : 'Home'
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            {this.state.page === 'Home' ?
+            'Home' :
+            `Home > ${this.state.page}`}
+          </h1>
         </header>
         <SideBar />
+        <Content />
         <footer className="App-footer">
           Chungnam National Universitity(CNU)
         </footer>
